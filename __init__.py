79b1555e6711f6b82bd63721c9f9d9c7a810c12c
计都 <fuyouawa@gmail.com>
2025-11-01 21:53:47 +0800

    调整

--- __init__.py
+++ __init__.py
@@ -1,3 +1,26 @@
+# Add custom API routes, using router
+from aiohttp import web
+from server import PromptServer
 
-from .easytoolkit import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+from nodes.example import *
+
+@PromptServer.instance.routes.get("/hello")
+async def get_hello(request):
+    return web.json_response("hello")
+
+
+# A dictionary that contains all nodes you want to export with their names
+# NOTE: names should be globally unique
+NODE_CLASS_MAPPINGS = {
+    "Example": Example
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Example": "Example Node"
+}
+
+
+NODES_DIRECTORY = "./nodes"
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'NODES_DIRECTORY']
--- nodes/example.py
+++ nodes/example.py
@@ -132,24 +132,3 @@ class Example:
 
 # Set the web directory, any .js file in that directory will be loaded by the frontend as a frontend extension
 # WEB_DIRECTORY = "./somejs"
-
-
-# Add custom API routes, using router
-from aiohttp import web
-from server import PromptServer
-
-@PromptServer.instance.routes.get("/hello")
-async def get_hello(request):
-    return web.json_response("hello")
-
-
-# A dictionary that contains all nodes you want to export with their names
-# NOTE: names should be globally unique
-NODE_CLASS_MAPPINGS = {
-    "Example": Example
-}
-
-# A dictionary that contains the friendly/humanly readable titles for the nodes
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "Example": "Example Node"
-}
