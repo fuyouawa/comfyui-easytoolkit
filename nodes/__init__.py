3dfaa4aefe4620acaf9b55a49acc29500ee40ee7
计都 <fuyouawa@gmail.com>
2025-11-05 11:13:11 +0800

    ✨ 更新：内置支持video_formats

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -1,3 +1,13 @@
+import folder_paths
+import os
+
+folder_paths.folder_names_and_paths["video_formats"] = (
+    [
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), ".", "video_formats"),
+    ],
+    [".json"]
+)
+
 from .algorithm import *
 from .image import *
 from .misc import *
