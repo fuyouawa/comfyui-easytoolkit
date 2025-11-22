4be3c34c23c44fc7113afb86c5c9b518d3dda157
计都 <fuyouawa@gmail.com>
2025-11-22 20:15:39 +0800

    🔥删除：冗余文件和代码

--- nodes/video/__init__.py
+++
@@ -1,9 +0,0 @@
-# import folder_paths
-# import os
-
-# folder_paths.folder_names_and_paths["video_formats"] = (
-#     [
-#         os.path.join(os.path.dirname(os.path.abspath(__file__)), ".", "video_formats"),
-#     ],
-#     [".json"]
-# )
